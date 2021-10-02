72b53b1bffdecb64b27cf8f6c5cc0f5398adaa09
360matt <mattbacq2004@gmail.com>
2021-10-02 22:59:22 +0200

    Push to sonatype

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Test {
 
     @org.junit.Test
-    public static void main (String[] args) {
+    public void main () {
 
         ExampleData configReflect = new ExampleData();
 
